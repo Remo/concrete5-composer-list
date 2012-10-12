4622cec83add78bddb7e8e26ebcd5daba593afff
Remo Laubacher <remo.laubacher@gmail.com>
2012-10-12 18:45:04 +0200

    cleanup

--- remo_composer_list/single_pages/dashboard/composer/list.php
+++ remo_composer_list/single_pages/dashboard/composer/list.php
@@ -1,28 +1,22 @@
 <?php
-
 defined('C5_EXECUTE') or die('Access Denied.');
 
 $nh = Loader::helper('navigation');
 $ih = Loader::helper('concrete/interface');
 
 echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(
-   t('Composer Page List'),
-   t('Shows a list of all pages editable by the composer.'),
-   'span10 offset1',
-   false);
+        t('Composer Page List'), t('Shows a list of all pages editable by the composer.'), 'span10 offset1', false);
 ?>
 
 <div class="ccm-pane-body">
 <?php
 
-echo t('You have not setup any page types for Composer.');
-
 if ($pages) {
     echo '<table class="table">';
     foreach ($pages as $page) {
-        
+
         $button = $ih->button(t('Edit'), View::url('/dashboard/composer/write/-/edit/', $page->getCollectionID()));
-                
+
         echo "
             <tr>
                 <th>{$page->getCollectionName()}</th>
@@ -32,18 +26,16 @@ if ($pages) {
     echo '</table>';
 
     echo $pagesPagination;
-}
-else {
+} else {
     if (count($composerCollectionTypes) > 0) {
         echo '<h3>' . t('What type of page would you like to edit?') . '</h3>';
         echo '<ul class="item-select-list">';
 
-        foreach($composerCollectionTypes as $collectionType) {
-            echo '<li class="item-select-page"><a href="' . 
-                    $this->action('show', $collectionType->getCollectionTypeID()) . '">' . $collectionType->getCollectionTypeName() . '</a></li>';
+        foreach ($composerCollectionTypes as $collectionType) {
+            echo '<li class="item-select-page"><a href="' .
+            $this->action('show', $collectionType->getCollectionTypeID()) . '">' . $collectionType->getCollectionTypeName() . '</a></li>';
         }
         echo '</ul>';
-
     } else {
         echo '<p>' . t('You have not setup any page types for Composer.') . '</p>';
     }
@@ -51,8 +43,5 @@ else {
 ?>
 </div>
 
-<?php
-
-
-
-echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false);
+    <?php
+    echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false);
