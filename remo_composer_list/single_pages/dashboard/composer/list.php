0a6189501a04fba3a9449fd1db6696eee1449754
naume.keculovski <naume.keculovski@mesch.ch>
2013-03-22 10:45:13 +0100

    made edit button and delete button in one line

--- remo_composer_list/single_pages/dashboard/composer/list.php
+++ remo_composer_list/single_pages/dashboard/composer/list.php
@@ -6,70 +6,29 @@ $ih = Loader::helper('concrete/interface');
 
 echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(
         t('Composer Page List'), t('Shows a list of all pages editable by the composer.'), 'span10 offset1', false);
-
-if ($pages && $displaySearchBox) {
-    ?>
-
-    <div class="ccm-pane-options">
-        <form class="form-horizontal" method="post">
-            <div class="ccm-pane-options-permanent-search">
-                <div class="span5">
-                    <label for="cvName" class="control-label">Page Name</label>		
-                    <div class="controls">
-                        <input id="cvName" type="text" name="cvName" value="" style="width: 120px" class="ccm-input-text">		
-                    </div>
-                    <?php
-                    echo $ih->submit(t('Search'), false, 'left', null, array('style'=>'margin-left: 10px;'));
-                    ?>
-                </div>
-            </div>     
-            <div class="clearfix ccm-pane-options-content"></div>
-        </form>
-    </div>
-
-<?php } ?>
+?>
 
 <div class="ccm-pane-body">
     <?php
     if ($pages) {
-        
-        $previousParentID = -1;
-        
+        echo '<table class="table">';
         foreach ($pages as $page) {
 
-            if ($previousParentID != $page->getCollectionParentID()) {
-                $previousParentID = $page->getCollectionParentID();
-                $parentPage = Page::getByID($previousParentID);
-                
-                if ($previousParentID > -1) {
-                    echo '</tbody></table>';
-                }
-                
-                echo "<table class=\"table composer-list-sortable\">";
-                if ($ctPublishMethod != 'PARENT') {
-                    echo "<thead><tr><th colspan=\"2\">{$parentPage->getCollectionName()}</th></tr></thead>";
-                }
-                echo "<tbody>";
-            }
-            
-            
-            $button = $ih->button(t('Edit'), View::url('/dashboard/composer/write/-/edit/', $page->getCollectionID()), '', 'right primary');
-            $button .= $ih->button(t('Delete'), View::url('/dashboard/composer/list/delete/', $ctID, $page->getCollectionID()), '', 'right', array(
+            $button_edit = $ih->button(t('Edit'), View::url('/dashboard/composer/write/-/edit/', $page->getCollectionID()), '', 'right primary');
+            $button_delete = $ih->button(t('Delete'), View::url('/dashboard/composer/list/delete/', $ctID, $page->getCollectionID()), '', 'right', array(
                 'style' => 'margin-left: 10px;',
                 'onclick' => 'return confirm(\'' . t('Are you sure you want to remove this page?') . '\')'
             ));
 
             echo "
-            <tr id=\"cID-{$page->getCollectionID()}\">
-                <th style=\"width: 30%;\">{$page->getCollectionName()}</th>
-                <td style=\"width: 50%;\">{$page->getCollectionPath()}</td>
-                <td style=\"width: 20%; text-align: right;\">{$button}</td>
+            <tr>
+                <th>{$page->getCollectionName()}</th>
+                <td>{$page->getCollectionPath()}</td>
+                <td style=\"text-align: right;width:70px;\">{$button_edit}</td>
+                <td style=\"text-align: right;width:70px;\">{$button_delete}</td>
             </tr>";
         }
-        
-        if ($previousParentID > -1) {
-            echo '</table>';
-        }
+        echo '</table>';
 
         echo $pagesPagination;
     } else {
