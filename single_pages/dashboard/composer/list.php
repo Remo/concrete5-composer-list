aa4e65725f4c5b6eee2a17e2e410d0206c3f587b
Remo Laubacher <remo.laubacher@gmail.com>
2014-01-09 09:31:18 +0100

    code indentation fixed

--- single_pages/dashboard/composer/list.php
+++ single_pages/dashboard/composer/list.php
@@ -5,13 +5,13 @@ $nh = Loader::helper('navigation');
 $ih = Loader::helper('concrete/interface');
 
 echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(
-        t($composerListTitel . ' ') . t('List'), t('Shows a list of all pages editable by the composer.'), 'span10 offset1', false);
+    t($composerListTitel . ' ') . t('List'), t('Shows a list of all pages editable by the composer.'), 'span10 offset1', false);
 
 if ($pages && $displaySearchBox || $emptyList) {
     ?>
 
     <div class="ccm-pane-options">
-        <form class="form-horizontal" action="<?php echo $this->action('show/' . $ctID); ?>" method="post">
+        <form class="form-horizontal" action="<?php echo $this->action('show/' . $ctID);?>" method="post">
             <div class="ccm-pane-options-permanent-search">
                 <div class="span5">
                     <label for="cvName" class="control-label">Page Name</label>		
@@ -27,69 +27,79 @@ if ($pages && $displaySearchBox || $emptyList) {
         </form>
     </div>
 
-<?php } ?>
+<?php }?>
 
 <div class="ccm-pane-body">
     <?php
     if ($pages) {
-        
+
         $previousParentID = -1;
-        
+
         foreach ($pages as $page) {
 
             if ($previousParentID != $page->getCollectionParentID()) {
                 $previousParentID = $page->getCollectionParentID();
                 $parentPage = Page::getByID($previousParentID);
-                
+
                 if ($previousParentID > -1) {
-                    echo '</tbody></table>';
+                    ?>
+                </tbody></table>
+            <?php }
+            ?>
+            <table class="table composer-list-sortable">
+                <?php if ($ctPublishMethod != 'PARENT') {?>
+                    <thead><tr><th colspan="4"><?php echo $parentPage->getCollectionName()?></th></tr></thead>
+                    <?php
                 }
-                
-                echo "<table class=\"table composer-list-sortable\">";
-                if ($ctPublishMethod != 'PARENT') {
-                    echo "<thead><tr><th colspan=\"4\">{$parentPage->getCollectionName()}</th></tr></thead>";
+                ?>
+                <tbody>
+                    <?php
                 }
-                echo "<tbody>";
+
+                $button_edit = $ih->button(t('Edit'), View::url('/dashboard/composer/write/-/edit/', $page->getCollectionID()), '', 'right primary');
+                $button_delete = $ih->button(t('Delete'), View::url('/dashboard/composer/list/delete/', $ctID, $page->getCollectionID()), '', 'right', array(
+                    'style' => 'margin-left: 10px;',
+                    'onclick' => 'return confirm(\'' . t('Are you sure you want to remove this page?') . '\')'
+                ));
+                ?>
+                <tr id="cID-<?php echo $page->getCollectionID()?>">
+                    <th style="width: 30%;"><?php echo $page->getCollectionName()?></th>
+                    <td style="width: 50%;"><?php echo $page->getCollectionPath()?></td>
+                    <td style="width: 20%; text-align: right;\"><?php echo $button_edit?></td>
+                        <td style="text-align: right;width:70px;\"><?php echo $button_delete?></td>
+                </tr>
+                <?php
             }
-            
-            
-            $button_edit = $ih->button(t('Edit'), View::url('/dashboard/composer/write/-/edit/', $page->getCollectionID()), '', 'right primary');
-            $button_delete = $ih->button(t('Delete'), View::url('/dashboard/composer/list/delete/', $ctID, $page->getCollectionID()), '', 'right', array(
-                'style' => 'margin-left: 10px;',
-                'onclick' => 'return confirm(\'' . t('Are you sure you want to remove this page?') . '\')'
-            ));
 
-            echo "
-            <tr id=\"cID-{$page->getCollectionID()}\">
-                <th style=\"width: 30%;\">{$page->getCollectionName()}</th>
-                <td style=\"width: 50%;\">{$page->getCollectionPath()}</td>
-                <td style=\"width: 20%; text-align: right;\">{$button_edit}</td>
-                <td style=\"text-align: right;width:70px;\">{$button_delete}</td>
-            </tr>";
-        }
-        
-        if ($previousParentID > -1) {
-            echo '</table>';
-        }
-
-        echo $pagesPagination;
-    } else if ($emptyList) {
-        echo t('No pages found.');
-    } else {
-        if (count($composerCollectionTypes) > 0) {
-            echo '<h3>' . t('What type of page would you like to edit?') . '</h3>';
-            echo '<ul class="item-select-list">';
-
-            foreach ($composerCollectionTypes as $collectionType) {
-                echo '<li class="item-select-page"><a href="' .
-                $this->action('show', $collectionType->getCollectionTypeID()) . '">' . $collectionType->getCollectionTypeName() . '</a></li>';
-            }
-            echo '</ul>';
-        } else {
-            echo '<p>' . t('You have not setup any page types for Composer.') . '</p>';
-        }
+            if ($previousParentID > -1) {
+                ?>
+        </table>
+        <?php
     }
-    ?>
+
+    echo $pagesPagination;
+} else if ($emptyList) {
+    echo t('No pages found.');
+} else {
+    if (count($composerCollectionTypes) > 0) {
+        ?>
+        <h3><?php echo t('What type of page would you like to edit?')?></h3>
+        <ul class="item-select-list">
+            <?php foreach ($composerCollectionTypes as $collectionType) {?>
+                <li class="item-select-page">
+                    <a href="<?php echo $this->action('show', $collectionType->getCollectionTypeID())?>"><?php echo $collectionType->getCollectionTypeName()?></a></li>
+                <?php
+            }
+            ?>
+        </ul>
+        <?php } else {
+        ?>
+        <p><?php echo t('You have not setup any page types for Composer.')?>
+        </p>
+        <?php
+    }
+}
+?>
 </div>
 
 <?php
